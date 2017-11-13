f3a5781bbb49f4396275d803fef360dfe614b682
Roryg <giannichan@gmail.com>
2017-11-13 14:37:30 +0000

    adds redis docker container to script

--- dockerize.sh
+++ dockerize.sh
@@ -10,10 +10,15 @@ echo "Rebuilding image"
 
 docker build -t openactive/dashboard .
 
-echo "Runnning container"
+echo "Runnning containers"
+
+docker run --name openactive-redis -d redis
 
 docker run -d --name openactive-dashboard -p 3000:3000 \
+  --link openactive-redis:redis \
   --mount type=bind,source="$(pwd)",target=/app \
   -e ENV_VAR_1="hello" openactive/dashboard
+
+docker ps
 
 echo "Done. Visit http://localhost:3000/ to view app"
