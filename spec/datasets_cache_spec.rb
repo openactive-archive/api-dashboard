bd47276f152e646511053f2c121a86084fc621f7
Roryg <giannichan@gmail.com>
2017-11-13 14:37:13 +0000

    datasets cache test

--- spec/datasets_cache_spec.rb
+++ spec/datasets_cache_spec.rb
@@ -1,4 +1,17 @@
 require 'datasets_cache'
 
 describe DatasetsCache do
+
+  describe ".update" do
+    it "it stores result of request" do
+      expect(DatasetsCache.update).to eql("OK")
+    end
+  end
+
+  describe ".all" do
+    it "retrieves a collection of datasets" do
+      expect(DatasetsCache.all.class).to eql(Array)
+    end
+  end
+
 end
