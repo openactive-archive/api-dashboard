1f5986da33339dfd53a5b17978183d21f03a4e75
Roryg <giannichan@gmail.com>
2017-11-13 17:35:04 +0000

    use spec helper and adds example data for test

--- spec/datasets_cache_spec.rb
+++ spec/datasets_cache_spec.rb
@@ -1,3 +1,4 @@
+require 'spec_helper'
 require 'datasets_cache'
 
 describe DatasetsCache do
@@ -10,11 +11,14 @@ describe DatasetsCache do
 
   describe ".all" do
     it "retrieves a collection of datasets" do
+      example = [{ "id" => "1", "title" => "my dataset title", "data_url" => "http://mywebsait.com/data" }]
+      Redis.current.set('datasets', example.to_json)
+
       datasets = DatasetsCache.all
       expect(datasets.class).to eql(Array)
       expect(datasets.size).to be > 0
       expect(datasets[0].class).to eql(Hash)
-      expect(datasets[0].keys).to include("id", "title", "data_url") 
+      expect(datasets[0].keys).to include("id", "title", "data_url")
     end
   end
 
