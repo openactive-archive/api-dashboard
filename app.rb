149da286eb7cc37b318e3f354b15d0c1ecbda09e
Roryg <giannichan@gmail.com>
2017-11-13 16:10:23 +0000

    sets up redis connection and cache in app

--- app.rb
+++ app.rb
@@ -1,17 +1,31 @@
-require 'sinatra'
+require 'sinatra/base'
 require 'openactive'
+require 'redis'
+require_relative 'lib/datasets_cache'
 
-set :bind, '0.0.0.0'
+class DashboardApp < Sinatra::Base
+  
+  REDIS_HOST = ENV['REDIS_HOST'] || '127.0.0.1'
+  REDIS_PORT = ENV['REDIS_PORT'] || '6379'
 
-get '/' do
-  "the time where this server lives is #{Time.now}
-    <br /><br />check out your <a href=\"/agent\">user_agent</a>"
+  configure do
+    set :bind, '0.0.0.0' 
+    set :redis, Redis.new(host: REDIS_HOST, port: REDIS_PORT)
+  end
+
+  get '/' do
+    datasets = DatasetsCache.all
+    erb :index, locals: { datasets: datasets }
+  end
+
+  get '/agent' do
+    "you're using #{request.user_agent}"
+  end
+
+  get '/test' do
+    erb :test
+  end
+
 end
 
-get '/agent' do
-  "you're using #{request.user_agent}"
-end
-
-get '/test' do
-  erb :test
-end
+DashboardApp.run!
