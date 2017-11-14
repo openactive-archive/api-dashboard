59afcc708ef3d9b6a8dd29db9611792025382021
Roryg <giannichan@gmail.com>
2017-11-14 15:29:01 +0000

    look out for heroku redis environment variables

--- app.rb
+++ app.rb
@@ -4,15 +4,22 @@ require 'redis'
 require_relative 'lib/datasets_cache'
 
 class DashboardApp < Sinatra::Base
-  
-  OA_REDIS_HOST = ENV['OA_REDIS_HOST'] || '127.0.0.1'
-  OA_REDIS_PORT = ENV['OA_REDIS_PORT'] || '6379'
 
   configure do
     set :bind, '0.0.0.0'
     enable :logging
     set :logging, Logger::INFO
-    set :redis, Redis.new(host: OA_REDIS_HOST, port: OA_REDIS_PORT)
+
+    if ENV["REDISTOGO_URL"]
+      uri = URI.parse(ENV["REDISTOGO_URL"])
+      oa_redis_host = uri.host
+      oa_redis_port = uri.port
+    else
+      oa_redis_host = ENV['OA_REDIS_HOST'] || '127.0.0.1'
+      oa_redis_port = ENV['OA_REDIS_PORT'] || '6379'
+    end
+
+    set :redis, Redis.new(host: oa_redis_host, port: oa_redis_port)
     Redis.current = settings.redis
   end
 
