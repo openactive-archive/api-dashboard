61feacdd3fd869f49e5b12b56a77f4ba90df92e9
Roryg <giannichan@gmail.com>
2017-11-21 11:42:50 +0000

    ensure we have example data before running view test

--- spec/app/dashboard_app_spec.rb
+++ spec/app/dashboard_app_spec.rb
@@ -7,6 +7,23 @@ describe DashboardApp do
     DashboardApp
   end
 
+  before(:all) do
+    example = { "mywebsait/opendata" => 
+      {
+        "dataset-site-url"=>"http://data.activenewham.org.uk/",
+        "title"=>"activeNewham Sessions",
+        "description"=>"Session data from the activenewham.org.uk site",
+        "publisher-name"=>"activeNewham",
+        "publisher-url"=>"https://www.activenewham.org.uk",
+        "data-url"=>"https://activenewham-openactive.herokuapp.com",
+        "documentation-url"=>"https://github.com/activenewham/opendata",
+        "license-name"=>"Creative Commons Attribution Licence (CC-BY v4.0)",
+        "license-url"=>"https://creativecommons.org/licenses/by/4.0/",
+      }
+    }
+    Redis.current.set('datasets', example.to_json)
+  end
+
   it "includes google analytics code from environment variable" do
     ENV["GOOGLE_ANALYTICS_CODE"] = 'UA-XYZXYZ-Y'
     get "/"
