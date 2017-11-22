7c8f03fdf488fc9a3fb0fe93b82ec9f7956ce18f
Roryg <giannichan@gmail.com>
2017-11-22 15:06:05 +0000

    adds coverage for available and unavailable endpoints

--- spec/availability_cache_spec.rb
+++ spec/availability_cache_spec.rb
@@ -2,10 +2,17 @@ require 'spec_helper'
 
 describe AvailabilityCache do
 
+  before(:all) do
+    Redis.current.set('datasets', load_fixture("single-dataset.json"))
+  end
+
+  before(:each) do
+    Redis.current.del('availability')
+  end
+
   describe ".update" do
     it "stores availability if there were no issues making requests" do    
-      Redis.current.del('availability')
-      Redis.current.set('datasets', load_fixture("single-dataset.json"))
+      WebMock.stub_request(:get, "https://activenewham-openactive.herokuapp.com").to_return(:status => 200)
 
       result = AvailabilityCache.update
       not_updated = Redis.current.get('availability').nil?
@@ -21,22 +28,31 @@ describe AvailabilityCache do
         puts "Availability cache was not updated"
         expect(not_updated).to be(true)
       end
-
-      Redis.current.del('availability')
     end
+
+    it "correctly flags unavailable endpoints" do
+      WebMock.stub_request(:get, "https://activenewham-openactive.herokuapp.com").to_return(:status => 500)
+      AvailabilityCache.update
+      availability = JSON.parse(Redis.current.get('availability'))
+      expect(availability["https://activenewham-openactive.herokuapp.com"]).to be false
+     end
+
+     it "correctly flags available endpoints" do
+      WebMock.stub_request(:get, "https://activenewham-openactive.herokuapp.com").to_return(:status => 200)
+      AvailabilityCache.update
+      availability = JSON.parse(Redis.current.get('availability'))
+      expect(availability["https://activenewham-openactive.herokuapp.com"]).to be true
+     end
   end
 
   describe ".all" do
     it "retrieves a collection of availabilities" do
       example = { "https://ourparks.org.uk/getSessions" => true }
       Redis.current.set('availability', example.to_json)
-
       availability = AvailabilityCache.all
       expect(availability.class).to eql(Hash)
       expect(availability.keys.size).to be > 0
       expect(availability["https://ourparks.org.uk/getSessions"]).to eql(true)
-
-      Redis.current.del('availability')
     end
   end
 
@@ -46,6 +62,7 @@ describe AvailabilityCache do
     end
 
     it "returns an HTTP response code" do
+      WebMock.stub_request(:get, "https://ourparks.org.uk/getSessions")
       response_code = AvailabilityCache.fetch("https://ourparks.org.uk/getSessions")
       is_http_code = (100..527).include?(response_code.to_i)
       expect(is_http_code).to be true
