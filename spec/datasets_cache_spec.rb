6fd529b741e3975ae7942e30c97dc7d6ec426757
Roryg <giannichan@gmail.com>
2017-11-28 10:04:21 +0000

    webmocked so removing uneeded conditionals in test

--- spec/datasets_cache_spec.rb
+++ spec/datasets_cache_spec.rb
@@ -12,22 +12,16 @@ describe DatasetsCache do
       result = DatasetsCache.update
       not_updated = Redis.current.get('datasets').nil?
 
-      expect(result).to be(true).or be(false)
-
-      if result
-        expect(not_updated).to be(false)
-        datasets = JSON.parse(Redis.current.get('datasets'))
-        metadata_keys = datasets[datasets.keys.sample].keys
-        expect(datasets.keys.size).to be > 0
-        expect(datasets.class).to eql(Hash)
-        expect(metadata_keys).to include("title", "dataset-site-url", "data-url", 
-          "publisher-name", "documentation-url", "license-name", 
-          "license-url")
-      else
-        puts "Datasets cache was not updated"
-        expect(not_updated).to be(true)
-      end
-
+      expect(result).to be(true)
+      expect(not_updated).to be(false)
+      datasets = JSON.parse(Redis.current.get('datasets'))
+      metadata_keys = datasets[datasets.keys.sample].keys
+      expect(datasets.keys.size).to be > 0
+      expect(datasets.class).to eql(Hash)
+      expect(metadata_keys).to include("title", "dataset-site-url", "data-url", 
+        "publisher-name", "documentation-url", "license-name", 
+        "license-url")
+   
       Redis.current.del('datasets')
     end
 
