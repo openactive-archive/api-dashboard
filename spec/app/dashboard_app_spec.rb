df7f694c634ae7ee286a23ae02eabe3d95675b78
Roryg <giannichan@gmail.com>
2017-11-28 11:44:48 +0000

    make sure uses-opportunity-model and uses-paging-spec are included in download

--- spec/app/dashboard_app_spec.rb
+++ spec/app/dashboard_app_spec.rb
@@ -38,7 +38,8 @@ describe DashboardApp do
     expect(result["data"].keys.size).to eql(2)
     expect(result["data"]["activenewham/opendata"]).to include("dataset-site-url", "title", "description", "publisher-name", 
       "publisher-url", "data-url", "documentation-url", "license-name", 
-      "license-url", "attribution-text", "attribution-url", "available")
+      "license-url", "attribution-text", "attribution-url", "available",
+      "uses-paging-spec", "uses-opportunity-model")
     expect(result["data"]["activenewham/opendata"]).not_to include('mailchimp', 'keyword-1', 'keyword-2', 'created', 'rpde-version', 
       'copyright-notice', 'odi-certificate-number', 'publish')
   end
