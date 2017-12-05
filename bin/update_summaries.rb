65288c1ad65e6c393468fe7aad6b035f293c245e
Roryg <giannichan@gmail.com>
2017-12-05 15:43:25 +0000

    adds restart option to update_summaries

--- bin/update_summaries.rb
+++ bin/update_summaries.rb
@@ -3,11 +3,18 @@ $:.unshift File.join( File.dirname(__FILE__), "..", "config")
 
 require 'environment'
 
+restart = ARGV.size > 0 and ARGV[0].eql?('--restart') 
 datasets = DatasetsCache.all
 
 for key in datasets.keys
   dataset = datasets[key]
   next unless dataset["uses-paging-spec"] and dataset["uses-opportunity-model"]
+
+  if restart
+    Redis.current.hdel(key, "activity_samples")
+    Redis.current.hdel(key, "last_page")
+  end
+
   summary = DatasetSummary.new(key)
   puts "\nHarvesting summary for #{key}, starting from #{summary.dataset_uri}"
   summary.harvest
