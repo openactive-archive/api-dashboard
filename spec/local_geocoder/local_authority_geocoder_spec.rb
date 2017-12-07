60b082b3d8cbf836e166063d3710e3e0e538b922
Roryg <giannichan@gmail.com>
2017-12-07 13:42:51 +0000

    adds out of bounds test

--- spec/local_geocoder/local_authority_geocoder_spec.rb
+++ spec/local_geocoder/local_authority_geocoder_spec.rb
@@ -9,6 +9,11 @@ describe LocalGeocoder::LocalAuthorityGeocoder do
       result = geocoder.reverse_geocode(-0.08660358, 51.27262669)
       expect(result.short_name).to eql "Tandridge"
     end
+
+    it "should return nil when unable to reverse geocode an authority" do
+      result = geocoder.reverse_geocode(0.0, 0.0)
+      expect(result).to eql nil
+    end
   end
 
 end
