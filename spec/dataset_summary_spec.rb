6a91bcd0f6672eb0cca6df420548ebf8982949ea
Roryg <giannichan@gmail.com>
2017-12-07 15:05:57 +0000

    fix typo

--- spec/dataset_summary_spec.rb
+++ spec/dataset_summary_spec.rb
@@ -109,8 +109,8 @@ describe DatasetSummary do
     it "returns an ordered list of boundaries" do
       activities = ["C", "A", "B", "A", "B", "A", "A"]
       activities.each { |a| Redis.current.zincrby("example/opendata/boundary", 1, a) }
-      expect(summary.ranked_activities).to eql(["A", "B", "C"])
-      expect(summary.ranked_activities(2)).to eql(["A", "B"])
+      expect(summary.ranked_boundaries).to eql(["A", "B", "C"])
+      expect(summary.ranked_boundaries(2)).to eql(["A", "B"])
     end
   end
 
