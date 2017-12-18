14822a91d22ca22302fea4b08a9a25f95eb935ed
Roryg <giannichan@gmail.com>
2017-12-18 14:56:03 +0000

    adds standard dataset reporting

--- lib/dashboard_metrics.rb
+++ lib/dashboard_metrics.rb
@@ -14,4 +14,17 @@ class DashboardMetrics
     CONX.metrics.create('dataset-count', self.dataset_count)
   end
 
+  def self.standard_datasets
+    count = 0
+    datasets = DatasetsCache.all
+    datasets.keys.each do |k|
+      count +=1 if datasets[k]["uses-opportunity-model"] and datasets[k]["uses-paging-spec"] 
+    end
+    count
+  end
+
+  def self.report_standard_datasets
+    CONX.metrics.create('standard-datasets', self.standard_datasets)
+  end
+
 end
--- spec/lib/dashboard_metrics_spec.rb
+++ spec/lib/dashboard_metrics_spec.rb
@@ -7,6 +7,8 @@ describe DashboardMetrics do
       to_return(:status => 200, :body => "")
     WebMock.stub_request(:post, "https://openactive-staging-metrics.herokuapp.com/metrics/dataset-count").
       to_return(:status => 201, :body => "")
+    WebMock.stub_request(:post, "https://openactive-staging-metrics.herokuapp.com/metrics/standard-datasets").
+      to_return(:status => 201, :body => "")
   end
 
   describe ".all" do
@@ -24,6 +26,31 @@ describe DashboardMetrics do
   describe ".report_dataset_count" do
     it "sends to bothan count of all datasets" do
       r = DashboardMetrics.report_dataset_count
+      expect(r.response.code).to eql("201")
+    end
+  end
+
+  describe ".standard_datasets" do
+    it "returns a count of all standard conforming datasets" do
+      example = { 
+        "mywebsait/opendata" => { 
+          "title" => "my dataset title", "data-url" => "http://mywebsait.com/data" 
+        }, "examplesite/opendata" => { 
+          "title" => "my other dataset", "data-url" => "http://example.com/data",
+          "uses-opportunity-model" => true, "uses-paging-spec" => true 
+        }, "moresites/opendata" => { 
+          "title" => "big dataset", "data-url" => "http://anothersite.com/data",
+          "uses-opportunity-model" => false, "uses-paging-spec" => true 
+        }
+      }
+      Redis.current.set('datasets', example.to_json)
+      expect(DashboardMetrics.standard_datasets).to eql(1)
+    end
+  end
+
+  describe ".report_standard_datasets" do
+    it "sends to bothan count of all standard conforming datasets" do
+      r = DashboardMetrics.report_standard_datasets
       expect(r.response.code).to eql("201")
     end
   end
